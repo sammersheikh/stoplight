3172b472307f99cb32bb11c59341127f608a315c
sammersheikh <sammer.sheikh@outlook.com>
2022-09-22 16:22:24 -0700

    basic functionality achieved

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-let stoplight
 
 const stop = document.getElementById('stop')
 const slow = document.getElementById('slow')
 const go = document.getElementById('go')
-const td = document.getElementsByTagName('td')
+// const tr = document.getElementsByName('tr')
+// const tds = tr.childNodes
+const tds = document.querySelectorAll('td')
+
+function resetStoplight() {
+tds.forEach(function(td) {
+    td.style.backgroundColor = 'gray'
+})
+}
 
 let stopBtn = document.getElementById('stopBtn')
 let slowBtn = document.getElementById('slowBtn')
 let goBtn = document.getElementById('goBtn')
 
 
-stopBtn.addEventListener('click', function(stoplight){
-    stoplight === 'stop'
-    console.log('stop')
-    stoplightRoutine(stoplight)
-})
-
-slowBtn.addEventListener('click', function(stoplight) {
-    stoplight === 'slow'
-    console.log('slow')
-    stoplightRoutine(stoplight)
-})
-
-function stoplightRoutine(stoplight) {
-
-if (stoplight === 'go') {
-    go.style.backgroundColor = 'green'
-} else if (stoplight === 'slow') {
-    slow.style.backgroundColor = 'yellow'
-} else if (stoplight === 'stop') {
+stopBtn.addEventListener('click', function() {
+    resetStoplight()
     stop.style.backgroundColor = 'red'
-}
+})
 
-}
+slowBtn.addEventListener('click', function() {
+    resetStoplight()
+    slow.style.backgroundColor = 'yellow'
+})
+
+goBtn.addEventListener('click', function() {
+    resetStoplight()
+    go.style.backgroundColor = 'green'
+})
+
+
+
+// function stoplightRoutine() {
+
+// if (stoplight === 'go') {
+//     go.style.backgroundColor = 'green'
+// } else if (stoplight === 'slow') {
+//     slow.style.backgroundColor = 'yellow'
+// } else if (stoplight === 'stop') {
+//     stop.style.backgroundColor = 'red'
+// }
+
+// }
