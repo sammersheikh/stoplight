a7b60655db8b3304bb4481cfccd62f9e2387c37b
sammersheikh <sammer.sheikh@outlook.com>
2022-09-22 15:18:00 -0700

    added more js functionality

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-
 let stoplight
 
 const stop = document.getElementById('stop')
 const slow = document.getElementById('slow')
 const go = document.getElementById('go')
+const td = document.getElementsByTagName('td')
 
 let stopBtn = document.getElementById('stopBtn')
+let slowBtn = document.getElementById('slowBtn')
+let goBtn = document.getElementById('goBtn')
 
-stopBtn.addEventListener('click', function(){
+
+stopBtn.addEventListener('click', function(stoplight){
     stoplight === 'stop'
-    stoplightRoutine()
+    console.log('stop')
+    stoplightRoutine(stoplight)
 })
 
-function stoplightRoutine() {
+slowBtn.addEventListener('click', function(stoplight) {
+    stoplight === 'slow'
+    console.log('slow')
+    stoplightRoutine(stoplight)
+})
+
+function stoplightRoutine(stoplight) {
 
 if (stoplight === 'go') {
     go.style.backgroundColor = 'green'
